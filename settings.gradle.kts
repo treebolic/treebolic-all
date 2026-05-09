ca52f1b8b329b9ade23dc477f2417ca1749f84f3
1313ou <1313ou@gmail.com>
2026-05-09 09:52:00 +0200

    house keeping

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,9 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    @Suppress("UnstableApiUsage")
     repositories {
         google()
         mavenLocal()
